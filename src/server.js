6908826561876b374610a8a1c4ba2e214c189343
bubbletea03 <bubbletea03@naver.com>
2023-01-11 23:25:24 +0900

    Setup frontend directories

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => res.render("home"));
 
 console.log("hello");
 
